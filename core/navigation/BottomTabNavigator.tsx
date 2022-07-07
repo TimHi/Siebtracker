135b4598413e063a0ff9b559dfbc61016f29f343
Tim Hiller <TimHiller@live.de>
2022-07-07 19:10:28 +0200

    adding bottom navigator with Icons

--- core/navigation/BottomTabNavigator.tsx
+++ core/navigation/BottomTabNavigator.tsx
@@ -4,6 +4,7 @@ import HomeScreen from '../../UI/screens/HomeScreen';
 import NewCoffeeScreen from '../../UI/screens/NewCoffeeScreen';
 import StatisticsScreen from '../../UI/screens/StatisticsScreen';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export type BottomTabStackParams = {
   Home: undefined;
@@ -16,9 +17,21 @@ export default function BottomTabNavigator() {
   return (
     <SafeAreaProvider>
       <BottomTabStack.Navigator
-        screenOptions={() => ({
+        screenOptions={({route}) => ({
           headerShown: false,
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
 
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'NewCoffee') {
+              iconName = focused ? 'grid' : 'grid-outline';
+            } else if (route.name === 'Statistics') {
+              iconName = focused ? 'md-cart' : 'md-cart-outline';
+            }
+
+            return <Icon name={iconName} size={size} color={color} />;
+          },
           tabBarActiveTintColor: 'tomato',
           tabBarInactiveTintColor: 'gray',
         })}>
